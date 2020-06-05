1d729225115fcddb90f9693dfa215c02a5358ab8
suvanshm <suvansh@manektala.com>
2020-06-05 16:28:26 +0530

    Merge sort added!

---
+++ SortAlgos/MergeSort.py
@@ -0,0 +1,27 @@
+def merge_sort(lst):
+    if len(lst) <= 1:
+        return lst
+
+    middle = len(lst) // 2
+    left = lst[:middle]
+    right = lst[middle:]
+    left_sort = merge_sort(left)
+    right_sort = merge_sort(right)
+
+    return merge(left_sort, right_sort)
+
+
+def merge(left, right):
+    result = []
+    while left and right:
+        if left[0] < right[0]:
+            result.append(left.pop(0))
+        else:
+            result.append(right.pop(0))
+    return result + left + right
+
+
+# Testing
+test = [8, 3, 5, 7, 1, 6, 9, 2]
+print(test)
+print(merge_sort(test))
