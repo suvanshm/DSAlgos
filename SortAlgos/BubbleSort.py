e1a32c6b641a7069f126e2d5518259e7562bd242
suvanshm <suvansh@manektala.com>
2020-06-05 12:47:54 +0530

    bubble sort updated

--- SortAlgos/BubbleSort.py
+++ SortAlgos/BubbleSort.py
@@ -1,6 +1,6 @@
 def bubble_sort(lst):
     n = len(lst)
-    for i in range(n):
+    for i in range(n-1):
         for j in range(n - i - 1):
             if lst[j] > lst[j + 1]:
                 lst[j + 1], lst[j] = lst[j], lst[j + 1]
