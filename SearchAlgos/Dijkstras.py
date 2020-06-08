e1d7270a1404135df7390a7ec5b2a2291da5947f
suvanshm <suvansh@manektala.com>
2020-06-08 13:30:06 +0530

    Added Dijkstra's algorithm.

---
+++ SearchAlgos/Dijkstras.py
@@ -0,0 +1,80 @@
+from DataStructures.Graph import Vertex, Graph
+from DataStructures.Heap import MinHeap
+from math import inf
+from heapq import heappop, heappush
+
+
+# Home made implementation
+def dijkstras(graph, start):
+    distances = {}
+    for vertex in graph.graph_dict.keys():
+        distances[vertex] = inf
+    distances[start] = 0
+    explore = MinHeap()
+    explore.add([0, start])
+    while explore:
+        try:
+            distance, current = explore.get_min()
+        except TypeError:
+            break
+        currentvertex = graph.graph_dict[current]
+        neighbours = currentvertex.get_neighbours()
+        for neighbour, weight in neighbours:
+            newdistance = distance + weight
+            if newdistance < distances[neighbour]:
+                distances[neighbour] = newdistance
+                explore.add([newdistance, neighbour])
+    return distances
+
+
+# Implementation using heapq
+def dijkstras2(graph, start):
+    distances = {}
+    for vertex in graph.graph_dict.keys():
+        distances[vertex] = inf
+    distances[start] = 0
+    explore = [(0, start)]
+    while explore:
+        distance, current = heappop(explore)
+        currentvertex = graph.graph_dict[current]
+        neighbours = currentvertex.get_neighbours()
+        for neighbour, weight in neighbours:
+            newdistance = distance + weight
+            if newdistance < distances[neighbour]:
+                distances[neighbour] = newdistance
+                heappush(explore, (newdistance, neighbour))
+    return distances
+
+
+#Testing
+delhi = Vertex("Delhi")
+mumbai = Vertex("Mumbai")
+chennai = Vertex("Chennai")
+kolkata = Vertex("Kolkata")
+srinagar = Vertex("Srinagar")
+imphal = Vertex("Imphal")
+surat = Vertex("Surat")
+bhopal = Vertex("Bhopal")
+jaipur = Vertex("Jaipur")
+india = Graph()
+india.add_vertex(delhi)
+india.add_vertex(mumbai)
+india.add_vertex(chennai)
+india.add_vertex(kolkata)
+india.add_vertex(srinagar)
+india.add_vertex(imphal)
+india.add_vertex(surat)
+india.add_vertex(bhopal)
+india.add_vertex(jaipur)
+india.add_edge(mumbai, chennai, 1.5)
+india.add_edge(mumbai, kolkata, 2.5)
+india.add_edge(mumbai, delhi, 2)
+india.add_edge(delhi, kolkata, 2.5)
+india.add_edge(delhi, chennai, 3)
+india.add_edge(chennai, kolkata, 2.5)
+india.add_edge(mumbai, surat, 0.5)
+india.add_edge(delhi, srinagar, 1.5)
+india.add_edge(kolkata, imphal, 1)
+india.add_edge(bhopal, jaipur, 1.5)
+
+print(dijkstras(india, "Delhi"))
